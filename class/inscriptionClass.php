9a29e45f89442d6bf99c325b4321d954608293f4
su apt install wayz <wayzzy59@gmail.com>
2023-01-18 20:02:02 +0100

    aaa

--- class/inscriptionClass.php
+++ class/inscriptionClass.php
@@ -17,11 +17,11 @@
             $this->valid = (boolean) true;
 
             if(isset($identifiant)) {
-                $verif = $DB->prepare("select id from utilisateurs where pseudo = ?");
+                $verif = $DB->prepare("select Code_personnel from personnel where Identifiant = ?");
                 $verif->execute(array($identifiant));
                 $verif=$verif->fetch();
 
-                if(isset($verif['id'])) {
+                if(isset($verif['Code_personnel'])) {
                     $this->valid = false;
                     $this->erreur = "Ce pseudo est déjà pris.";
                 }
@@ -29,42 +29,8 @@
                 if($this->valid) {
                     $crypt_password = password_hash($password, PASSWORD_ARGON2ID);
 
-                    switch (random_int(1,8)) {
-                        case 1:
-                            $banner = 'default_banner_1.jpg';
-                        break;
-        
-                        case 2:
-                            $banner = 'default_banner_2.jpg';
-                        break;
-        
-                        case 3:
-                            $banner = 'default_banner_3.jpg';
-                        break;
-        
-                        case 4:
-                            $banner = 'default_banner_4.jpg';
-                        break;
-
-                        case 5:
-                            $banner = 'default_banner_5.jpg';
-                        break;
-        
-                        case 6:
-                            $banner = 'default_banner_6.jpg';
-                        break;
-        
-                        case 7:
-                            $banner = 'default_banner_7.jpg';
-                        break;
-        
-                        case 8:
-                            $banner = 'default_banner_8.jpg';
-                        break;
-                    }
-
-                    $insert_user = $DB->prepare("INSERT INTO utilisateurs (pseudo, email, banniere, password) VALUES(?, ?, ?, ?)");
-                    $insert_user->execute(array($identifiant, $mail, $banner, $crypt_password));
+                    $insert_user = $DB->prepare("INSERT INTO personnel (Nom, Identifiant, Mot_de_passe, role) VALUES(?, ?, ?, 'secretaire')");
+                    $insert_user->execute(array($identifiant, $identifiant, $crypt_password));
                     
                 }
             }
--- register.php
+++ register.php
@@ -57,17 +57,7 @@
             </form>
         </div>
         <div class="right">
-            <div class="desc">
-                <div class="slogan">
-                    <div class="slog-item">
-                        <svg class="play-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
-                        <h1>La plateforme</h1>
-                    </div>
-                    <div class="slog-item"><h1>de stockage</h1></div>
-                    <div class="slog-item"><h1><span>en ligne.</span></h1></div>
-                    
-                </div>
-            </div>
+            <img src="./images/public/ICONE.svg" alt="">
         </div>
 
         <script src="./js/password.js"></script>
