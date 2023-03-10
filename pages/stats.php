8fccd48e393ced7163dd5ed86b2a9f201163122a
su apt install wayz <wayzzy59@gmail.com>
2023-03-10 10:33:44 +0100

    stats

--- pages/stats.php
+++ pages/stats.php
@@ -61,21 +61,76 @@
     <section class="global">
         <h1>Bon retour <?= htmlspecialchars($_SESSION['personnel'][2])?> 👋</h1>
         <div class="panel">
-            <p>Pour cette semaine :</p>
-            <div class="cards">
+
+            <div class="stats">
                 <?php
                     foreach ($stats as $liste) {
                         $libelle[] = $liste['libelle'];
                         $nbre[] = $liste['nbr_patient'];
+                    } 
                 ?>
-                <div class="card">
-                    <h2><?= $liste['libelle']?></h2>
-                    <h5 class="prof">Service</h5>
-                    <h5 class="count"><p><?= $liste['nbr_patient']?> <span>hospitalisation(s)</span></p></h5>
+                <div>
+                    <canvas id="myChart"></canvas>
                 </div>
-                <?php
-                    }
-                ?>
+                <div>
+                    <canvas id="myChart2"></canvas>
+                </div>
+
+                <script>
+                    const ctx = document.getElementById('myChart');
+
+
+                    new Chart(ctx, {
+                        type: 'doughnut',
+                        data: {
+                        labels: <?= json_encode($libelle)?>,
+                        datasets: [{
+                            label: 'Hospitalisations',
+                            data: <?= json_encode($nbre)?>,
+                            borderWidth: 1,
+                            hoverOffset: 4
+                        }]
+                        },
+
+                    
+                        scales: {
+
+                        yAxes: [{
+                        beginAtZero: true,
+                        gridLines:{
+                        display:false
+                        }
+                        }]
+
+                        }
+                    });
+
+                    const ctx2 = document.getElementById('myChart2');
+
+
+                    new Chart(ctx2, {
+                        type: 'bar',
+                        data: {
+                        labels: <?= json_encode($libelle)?>,
+                        datasets: [{
+                            label: 'Hospitalisations',
+                            data: <?= json_encode($nbre)?>,
+                            backgroundColor: ['#3246D3'],
+                            borderWidth: 1,
+                            hoverOffset: 4
+                        }]
+                        },
+
+                    
+                        options: {
+                        scales: {
+                            y: {
+                            beginAtZero: true
+                            }
+                        }
+                        }
+                    });
+                </script>
             </div>
         </div>
     </section>
